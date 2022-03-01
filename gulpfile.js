6c68c5ffdfdf56b310213542e6b3015d6eba250a
Vít Kilián <kilianv@spseplzen.cz>
2022-03-01 11:30:41 +0100

    removed gulp deploy

--- gulpfile.js
+++ gulpfile.js
@@ -10,17 +10,6 @@ const imagewebp = require('gulp-webp');
 const fileinclude = require('gulp-file-include');
 
 // create functions
-
-var gulp        = require('gulp');
-var deploy      = require('gulp-gh-pages');
-
-/**
- * Push build to gh-pages
- */
-gulp.task('deploy', function () {
-  return gulp.src("./build/**/*")
-    .pipe(deploy())
-});
   
 // scss
 function compilescss(){
