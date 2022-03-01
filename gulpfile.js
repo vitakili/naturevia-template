738cc0dd9ee113673534a04f3dc049f68ae2c599
Vít Kilián <kilianv@spseplzen.cz>
2022-03-01 11:18:20 +0100

    gulp deploy

--- gulpfile.js
+++ gulpfile.js
@@ -11,6 +11,16 @@ const fileinclude = require('gulp-file-include');
 
 // create functions
 
+var gulp        = require('gulp');
+var deploy      = require('gulp-gh-pages');
+
+/**
+ * Push build to gh-pages
+ */
+gulp.task('deploy', function () {
+  return gulp.src("./dist/**/*")
+    .pipe(deploy())
+});
   
 // scss
 function compilescss(){
