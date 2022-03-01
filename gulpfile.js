13683f5bcc1af5960902f93e536ca3792e5964ea
Vít Kilián <kilianv@spseplzen.cz>
2022-03-01 11:23:18 +0100

    publish

--- gulpfile.js
+++ gulpfile.js
@@ -18,7 +18,7 @@ var deploy      = require('gulp-gh-pages');
  * Push build to gh-pages
  */
 gulp.task('deploy', function () {
-  return gulp.src("./dist/**/*")
+  return gulp.src("./build/**/*")
     .pipe(deploy())
 });
   
